aa51997e20a7641550667b3f58468a957f4bc9e4
Ashish Aggarwal <asaggarwal@DELC02T84M5G8WN.sea.corp.expecn.com>
2018-07-03 12:10:15 +0530

    improving the release process for npm module

---
+++ scripts/version.js
@@ -0,0 +1,27 @@
+const pkginfo = require('npm-registry-package-info');
+const semver = require('semver');
+const fs = require('fs');
+
+var opts = {
+    'packages': [
+        'haystack-client'
+    ]
+};
+
+pkginfo(opts, (error, data) => {
+    if (error) {
+        throw error;
+    }
+    const currentVersion = data.data['haystack-client']['dist-tags']['latest'];
+    const releaseVersion = process.env.TRAVIS_TAG
+    if (!releaseVersion) {
+        throw new Error('No git tag commit is found, fail to release the "haystack-client" module');
+    }
+    if (semver.lte(releaseVersion, currentVersion)) {
+        throw new Error(`Current haystack-client on npm registry has greater than or equal new release version ${releaseVersion}. Check your git tag version`);
+    }
+    const rawPackageJson = fs.readFileSync('package.json');
+    const packageJson = JSON.parse(rawPackageJson);
+    packageJson.version = releaseVersion;
+    fs.writeFileSync('package.json', JSON.stringify(packageJson, null, ' '));
+});
